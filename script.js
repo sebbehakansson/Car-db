15de7f44148a7c41bc7997ae6a1d3bdc6578e30f
sebbehakansson <sebastian.sh74@gmail.comm>
2023-01-04 19:54:26 +0100

    början till programmet

--- script.js
+++ script.js
@@ -1,1 +1,39 @@
-ECHO is on.
+// ECHO is on.
+'use strict';
+
+let carContent = document.getElementById("carShowcase");
+
+const carObject = {
+    cars: [
+        {
+            make: 'BMW',
+            year: '2005',
+            fule: 'petrol',
+            drive: 'rwd',
+        },
+        {
+            make: 'Audi',
+            year: '2019',
+            fule: 'disel',
+            drive: 'awd',
+        },
+        {
+            make: 'tesla',
+            year: '2022',
+            fule: 'electric',
+            drive: 'rwd',
+        },
+        {
+            make: 'mercedes',
+            year: '2020',
+            fule: 'petrol',
+            drive: 'rwd',
+        },
+        {
+            make: 'volvo',
+            year: '1995',
+            fule: 'petrol',
+            drive: 'fwd',
+        },
+    ]
+}
